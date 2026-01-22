df82978e3a17b60ac952460290f0bc9f5dc976a4
404NotAFriend <brunodrcgomes@gmail.com>
2026-01-22 00:07:03 +0000

    new function ft_atol.c , atoi but for long numbers , updated makefile and headerfile

---
+++ ft_atol.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 00:04:27 by bramalho@st       #+#    #+#             */
+/*   Updated: 2026/01/22 00:04:32 by bramalho@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(const char *nptr)
+{
+	size_t	i;
+	long	nbr;
+	int		sign;
+
+	i = 0;
+	nbr = 0;
+	sign = 1;
+	while ((nptr[i] == ' ') || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		nbr = nbr * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (nbr * sign);
+}
